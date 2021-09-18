b0dc1dcea48fc3915f634d89c5b99601f7033256
Sonjh1306 <yale1306@gmail.com>
2021-09-18 13:26:33 +0900

    feat: 프로젝트 생성 및 초기 세팅

---
+++ VaccinationCenter/VaccinationCenterTests/VaccinationCenterTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+@testable import VaccinationCenter
+
+class VaccinationCenterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+       
+    }
+
+    func testExample() throws {
+    
+    }
+
+    func testPerformanceExample() throws {
+        
+        self.measure {
+        
+        }
+    }
+
+}
