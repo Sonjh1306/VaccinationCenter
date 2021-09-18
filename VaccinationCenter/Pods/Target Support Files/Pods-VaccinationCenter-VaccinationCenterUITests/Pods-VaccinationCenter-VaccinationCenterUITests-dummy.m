510065d94577300c802cf73a2f4d8ba5a0f3a06e
Sonjh1306 <yale1306@gmail.com>
2021-09-18 13:31:33 +0900

    feat: 라이브러리 추가

---
+++ VaccinationCenter/Pods/Target Support Files/Pods-VaccinationCenter-VaccinationCenterUITests/Pods-VaccinationCenter-VaccinationCenterUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VaccinationCenter_VaccinationCenterUITests : NSObject
+@end
+@implementation PodsDummy_Pods_VaccinationCenter_VaccinationCenterUITests
+@end
