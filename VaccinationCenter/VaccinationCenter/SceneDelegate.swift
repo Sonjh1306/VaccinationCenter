94658a836c53a6464dc96f32158f5f5b132d12e0
Sonjh1306 <yale1306@gmail.com>
2021-09-18 23:47:24 +0900

    feat: 스토리보드 제거 후 SceneDelegate 수정

--- VaccinationCenter/VaccinationCenter/SceneDelegate.swift
+++ VaccinationCenter/VaccinationCenter/SceneDelegate.swift
@@ -3,5 +3,14 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let mainViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
 }
 
